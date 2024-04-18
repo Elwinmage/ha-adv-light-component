e51d5aee9224903fa0b5eda86fb35dfb243be6b5
Elwinmage <52315693+Elwinmage@users.noreply.github.com>
2024-04-18 18:34:28 +0200

    Update __init__.py

--- custom_components/advlight/__init__.py
+++ custom_components/advlight/__init__.py
@@ -1,5 +1,4 @@
-"""
-Custom integration to integrate Telerupteurs with Home Assistant.
+"""Custom integration to integrate Telerupteurs with Home Assistant.
 
 For more details about this integration, please refer to
 https://github.com/Elwinmage/ha-adv-light-component
@@ -9,17 +8,14 @@ import logging
 
 from homeassistant.core import Config, HomeAssistant
 
-from .const import (
-    DOMAIN,
-    STARTUP_MESSAGE,
-)
+from .const import DOMAIN, STARTUP_MESSAGE
 
 SCAN_INTERVAL = timedelta(seconds=30)
 
 _LOGGER: logging.Logger = logging.getLogger(__package__)
 
 
-async def async_setup(hass: HomeAssistant, config: Config):
+async def async_setup(hass: HomeAssistant, Config: Config) -> bool:
     """Set up this integration using YAML is not supported."""
     if hass.data.get(DOMAIN) is None:
         hass.data.setdefault(DOMAIN, {})
